1872c77698af7df3602d15faa0813ba629f153dc
Sam Nazarko <email@samnazarko.co.uk>
2015-04-04 16:38:01 +0100

    Fix for "time might prevent reproducible builds" in GCC 4.9
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- os_dep/linux/pci_intf.c
+++ os_dep/linux/pci_intf.c
@@ -1963,7 +1963,6 @@ static int __init rtw_drv_entry(void)
 
 	RT_TRACE(_module_hci_intfs_c_,_drv_err_,("+rtw_drv_entry\n"));
 	DBG_871X("rtw driver version=%s\n", DRIVERVERSION);
-	DBG_871X("Build at: %s %s\n", __DATE__, __TIME__);
 	pci_drvpriv.drv_registered = _TRUE;
 
 	rtw_suspend_lock_init();
